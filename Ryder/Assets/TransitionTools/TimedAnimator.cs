6400072c1fba96915a03523266ecf1e41e577889
Kyler Alford <alfordkyler@gmail.com>
2020-01-26 11:38:24 -0500

    Logo Screen
    Set up animations for logo screen, and created a timed animator for every subsequent screen.

---
+++ Ryder/Assets/TransitionTools/TimedAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedAnimator : MonoBehaviour
+{
+	public StringReference onEnableAnimation;
+    public FloatReference animationBeginTime;
+	private Animator animator;
+    private float timer;
+
+	void Awake()
+	{
+		animator = gameObject.GetComponent<Animator>();
+	}
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > animationBeginTime.Value)
+        {
+            animator.SetTrigger(onEnableAnimation.Value);
+
+            timer = 0;
+
+            enabled = false;
+        }
+    }
+}
