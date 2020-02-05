ccaa2a6592ee61b4a64c49adbe6f660c8a8d3017
kyleralford <alfordkyler@gmail.com>
2020-02-05 10:25:16 -0500

    Currently Ryding Art Implementation
    Put in the new artwork for the "currently ryding" scene.

---
+++ Ryder/Assets/TransitionTools/StreetNameCycle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StreetNameCycle : MonoBehaviour
+{
+    [Tooltip("Time for each street name, in seconds")]
+    public FloatReference displayTime;
+    [Tooltip("Adds randomness from 0 to this value to display time")]
+    public FloatReference displayTimeRandom;
+    public string[] streetNames;
+    private TextMeshProUGUI textMesh;
+    private float timer;
+    private float displayTimeActual;
+
+    void Awake()
+    {
+        textMesh = gameObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    private void OnEnable()
+    {
+        RandomStreetName();
+        CalculateDisplayTime();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > displayTimeActual)
+        {
+            timer -= displayTimeActual;
+            RandomStreetName();
+            CalculateDisplayTime();
+        }
+    }
+
+    void RandomStreetName()
+    {
+        textMesh.text = streetNames[Random.Range(0, (streetNames.Length - 1))];
+    }
+
+    void CalculateDisplayTime()
+    {
+        displayTimeActual = displayTime.Value + Random.Range(0.0f, displayTimeRandom.Value);
+    }
+}
