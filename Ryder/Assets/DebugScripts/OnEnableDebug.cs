a7698640503944949eecd4837771793a45e0717c
kyleralford <alfordkyler@gmail.com>
2020-02-28 16:06:44 -0500

    New Scenes
    Added three or so new scenes, as well as a hamburger menu, to the app, per the most recent revisions.

---
+++ Ryder/Assets/DebugScripts/OnEnableDebug.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnEnableDebug : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        Debug.Log(gameObject.name + " was Enabled at time " + Time.time);
+    }
+}
